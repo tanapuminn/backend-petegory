60d11021078719d5683a6b5ed40af08f71471ae7
tanapuminn <tanapumin.fo@gmail.com>
2023-06-22 23:06:16 +0700

    create api

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const mysql = require('mysql')
 const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 
 const app = express()
-app.use(cors(
-    {
-        origin: ['http://localhost:3000'],
-        methods: ['POST', 'GET', 'PUT', 'DELETE'],
-        credentials: true
-    }
-))
+// app.use(cors(
+//     {
+//         origin: ['http://localhost:3000'],
+//         methods: ['POST', 'GET', 'PUT', 'DELETE'],
+//         credentials: true
+//     }
+// ))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 
 
@@ -46,6 +48,21 @@ app.post('/signup', (req,res) => {
             if (err) return res.json({ Error: 'Inside singup query' })
             return res.json({ Status: 'Success' })
         })
+    })
+})
+
+app.post('/login', (req,res) => {
+    const sql = 'SELECT * FROM users WHERE email = ? ';
+    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+        if(err) return res.json({Status: 'Error', Error: 'Error in running query'})
+        if(result.length > 0) {
+            bcrypt.compare(req.body.password.toString(), result[0].password, (err, response) => {
+                if (err) return res.json({ Error: "password error" });
+            })
+            return res.json({Status: 'Success'})
+        } else {
+            return res.json({Status: 'Error', Error: 'Wrong Email or Password!!'})
+        }
     })
 })
 
